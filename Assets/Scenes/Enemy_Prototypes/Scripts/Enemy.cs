58577cdbcd04b8c81843b1bd06b0fc405b1f913d
b1017023 <b1017023@fun.ac.jp>
2019-03-05 19:51:17 +0900

    敵キャラのプロトタイプを追加

---
+++ Assets/Scenes/Enemy_Prototypes/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject EnemyPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.gameObject.tag == "Player")
+        {
+            Instantiate(EnemyPrefab, new Vector3(Random.Range(-20, 20), 1, 0), new Quaternion(0, 0, 0, 0));
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scenes/Enemy_Prototypes/Scripts/EnemyEye.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyEye : MonoBehaviour
+{
+    public int vision = 5;//視界
+    public GameObject target;//標的
+    public float speed = 1;
+    SphereCollider scope;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scope = GetComponent<SphereCollider>();
+        rb = GetComponentInParent<Rigidbody>();//親のRigidbody
+        //Debug.Log(rb);
+        scope.radius = this.vision;//視界の反映
+        this.target = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {//標的を見つけているなら、
+            rb.useGravity = false;
+            Vector3 position = (target.transform.position - this.transform.position);
+            rb.AddForce(position * speed);
+            //Debug.Log(position);
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+            rb.useGravity = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            this.target = other.gameObject;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            this.target = null;
+        }
+    }
+
+}
